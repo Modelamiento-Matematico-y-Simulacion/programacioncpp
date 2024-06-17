a64b61239258e65281dd6eb8710e2a4a23a13a57
CrIstianBrAvo2004 <cbravon@unal.edu.co>
2024-06-17 18:55:53 -0500

    Uploading task about order of numbers using topics learned

---
+++ task_order-argument/build/CMakeFiles/task_order-argument.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task_order-argument.dir/src/argument.cpp.o"
+  "CMakeFiles/task_order-argument.dir/src/argument.cpp.o.d"
+  "task_order-argument"
+  "task_order-argument.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task_order-argument.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- task_order-argument/src/argument.cpp
+++ task_order-argument/src/argument.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
 #include <cmath>
 
-int main () {
+int main (int argC, char* argv[]) {
+
+	//Imprime los argumentos que se pasen por consola
+	std::cout << "Cantidad de números resgitrados: " << (argC - 1) << std::endl;
+	std::cout << "Los números son: " << std::endl;
+
+	for (int i = 1; i <= 3 && < argC; i++) {
+	std::cout << argv[i] << std::endl;
+
+	}
+	return 0;
+}
